283661a1ceede7993cb959fc123c3a6fae35605e
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-05-12 15:48:41 +0200

    add output numbers, fixed info check, ex completed

--- js/myscript.js
+++ js/myscript.js
@@ -1,15 +1,41 @@
+/* Variables Declaring & Initializing */
 const kilometersNumber = parseInt(prompt("Inserisci il numero di km da percorrere"));
 const passengerAge = parseInt(prompt("Inserisci l'età del passeggero"));
-let ticketDiscount;
-if (isNaN(kilometersNumber) || isNaN(passengerAge)) {
-    document.getElementById("output").innerHTML = "Errore. Le informazioni inserite non sono valide.";
-}
+let ticketDiscount = 0;
+let ticketFinalPrice;
+let discountPercent = "0%";
+
+/* Discounts Calculation */
 let ticketPrice = (kilometersNumber * 0.21);
 if (passengerAge < 18) {
+    discountPercent = "20%";
     ticketDiscount = ((20 / 100) * ticketPrice);
-    ticketPrice = (ticketPrice - ticketDiscount);
+    ticketFinalPrice = (ticketPrice - ticketDiscount);
 } else if (passengerAge > 65) {
+    discountPercent = "40%";
     ticketDiscount = ((40 / 100) * ticketPrice);
-    ticketPrice = (ticketPrice - ticketDiscount);
+    ticketFinalPrice = (ticketPrice - ticketDiscount);
+} else {
+    ticketFinalPrice = ticketPrice;
 }
-console.log(passengerAge, ticketPrice, ticketDiscount);
+
+/* Output Values Transformation */
+    ticketDiscount = ticketDiscount.toFixed(2);
+    ticketFinalPrice = ticketFinalPrice.toFixed(2);
+
+/* Output - Only if the inserted infos are valid */
+if (!(isNaN(kilometersNumber)) || (!isNaN(passengerAge))) {
+    document.getElementById("kilometersOutput").innerHTML = " " + kilometersNumber + ".";
+    document.getElementById("passengerAgeOutput").innerHTML = " " + passengerAge + ".";
+    document.getElementById("priceOutput").innerHTML = " " + ticketPrice + " €.";
+    document.getElementById("discountPercentOutput").innerHTML = " " + discountPercent;
+    document.getElementById("discountPriceOutput").innerHTML = " " + ticketDiscount + " €.";
+    document.getElementById("finalPriceOutput").innerHTML = " " + ticketFinalPrice + " €.";
+} else {
+    document.getElementById("kilometersOutput").innerHTML = " "+ "Valori inseriti non validi!"
+    document.getElementById("passengerAgeOutput").innerHTML = " " + "Valori inseriti non validi!"
+    document.getElementById("priceOutput").innerHTML = " " + "Valori inseriti non validi!"
+    document.getElementById("discountPercentOutput").innerHTML = " " + "Valori inseriti non validi!"
+    document.getElementById("discountPriceOutput").innerHTML = " " + "Valori inseriti non validi!"
+    document.getElementById("finalPriceOutput").innerHTML = " " + "Valori inseriti non validi!"
+}
