b2d42f6c93b19a54d8ef460b9abe12e0a9c31908
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-05-12 15:05:04 +0200

    added discount calculation if and else if

--- js/myscript.js
+++ js/myscript.js
@@ -1,5 +1,15 @@
 const kilometersNumber = parseInt(prompt("Inserisci il numero di km da percorrere"));
 const passengerAge = parseInt(prompt("Inserisci l'età del passeggero"));
+let ticketDiscount;
 if (isNaN(kilometersNumber) || isNaN(passengerAge)) {
     document.getElementById("output").innerHTML = "Errore. Le informazioni inserite non sono valide.";
 }
+let ticketPrice = (kilometersNumber * 0.21);
+if (passengerAge < 18) {
+    ticketDiscount = ((20 / 100) * ticketPrice);
+    ticketPrice = (ticketPrice - ticketDiscount);
+} else if (passengerAge > 65) {
+    ticketDiscount = ((40 / 100) * ticketPrice);
+    ticketPrice = (ticketPrice - ticketDiscount);
+}
+console.log(passengerAge, ticketPrice, ticketDiscount);
